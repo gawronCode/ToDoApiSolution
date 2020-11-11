3003edc2be0938602135c04d420e747e9f9c45e0
gawronCode <gawronCode@gmail.com>
2020-11-11 16:49:40 +0100

    Created Models for Db

---
+++ ToDoApi/Models/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoApi.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(128)]
+        public string Name { get; set; }
+
+        [Required]
+        public string AccountPassword { get; set; }
+
+    }
+}
---
+++ ToDoApi/Models/PlannedTask.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoApi.Models
+{
+    public class PlannedTask
+    {
+
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(2137)]
+        public string Description { get; set; }
+
+        public DateTime? DueDate { get; set; }
+
+        [Required]
+        public int StateId { get; set; }
+
+        public State State { get; set; }
+
+        [Required]
+        public int PersonId { get; set; }
+
+        public Person Person { get; set; }
+
+    }
+}
