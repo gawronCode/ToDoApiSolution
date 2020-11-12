f7c77ff28366b7a7af6b32503ff28ee6350246f9
gawronCode <gawronCode@gmail.com>
2020-11-12 13:06:26 +0100

    Add State type migartion

---
+++ ToDoApi/Migrations/20201112115436_AddStateTypes.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using ToDoApi.Models;
+
+
+namespace ToDoApi.Migrations
+{
+    public partial class AddStateTypes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData("State", "Name", "To Do");
+            migrationBuilder.InsertData("State", "Name", "In Progress");
+            migrationBuilder.InsertData("State", "Name", "Done");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData("State", "Name", "To Do");
+            migrationBuilder.DeleteData("State", "Name", "In Progress");
+            migrationBuilder.DeleteData("State", "Name", "Done");
+        }
+    }
+}
